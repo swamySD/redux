47081b1667fa02992b782ffa3e2196f033864575
Swamy <swamykumar539@gmail.com>
2023-06-15 15:56:11 +0530

    test

--- src/actions/addProduct.js
+++ src/actions/addProduct.js
@@ -4,7 +4,6 @@ export const addPostsData=()=>{
     // https://redux-384e0-default-rtdb.firebaseio.com/cart.json
      axios.post('https://fakestoreapi.com/products',{
         method:"POST",
-       
         body:JSON.stringify(
             {
                 title: 'test product',
@@ -15,18 +14,13 @@ export const addPostsData=()=>{
             }
         ) 
      })
-                 .then(res=>res.data)
-                 .then(jsonData=>{
-                     dispatch({type:'LOADED',payload:jsonData})
-                 }).catch(error=>{
-                     dispatch({type:'ERROR',payload:error})
-             })
-     
-     }
+    .then(res=>res.data)
+    .then(jsonData=>{
+        dispatch({type:'LOADED',payload:jsonData})
+        }).catch(error=>{
+            dispatch({type:'ERROR',payload:error})
+        })
+    }
  }
-     
-     
-     
- //store.dispatch(getPostsData)
- 
+//store.dispatch(getPostsData)
  export default  addPostsData; 
